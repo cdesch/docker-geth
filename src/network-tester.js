29cd7b12ef3c247e8026f128118ced2760d1be77
Chris <cdesch@gmail.com>
2021-04-13 23:56:57 -0400

    adding websockets tests

--- src/network-tester.js
+++ src/network-tester.js
@@ -9,10 +9,19 @@ const Web3 = require("web3");
 // const NETWORK_ADDRESS = "http://localhost:30356";
 
 const NETWORK_ADDRESS = "http://192.168.1.180:30356";
-const NETWORK_ADDRESS_WS = "ws://localhost:31037";
+const NETWORK_ADDRESS_WS = "ws://192.168.1.180:31037";
 
+async function main() {
 
-const web3 = new Web3(new Web3.providers.HttpProvider(NETWORK_ADDRESS));
+  const web3 = new Web3(new Web3.providers.HttpProvider(NETWORK_ADDRESS));
+  await web3.eth.net.isListening().then(() => console.log("connected")).catch((e) => console.log("error", e));
 
-web3.eth.net.isListening().then(() => console.log("connected")).catch((e) => console.log("error", e));
+  const web3ws = new Web3(new Web3.providers.WebsocketProvider(NETWORK_ADDRESS_WS));
+  await web3ws.eth.net.isListening().then(() => console.log("connected")).catch((e) => console.log("error", e));
+}
 
+main().then(() => {
+  console.log("done");
+  process.exit(0);
+}
+).catch(console.error);
